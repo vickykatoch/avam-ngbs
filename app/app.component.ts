29fa5bc55b60be992638f9e04768e9ea92b9a1e5
Balwinder Katoch <Balwinder@Balwinders-MacBook-Pro.local>
2016-12-11 06:10:30 -0500

    initial setup

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avam-ngbs',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Angular2 with Bootstrap 4'; }
